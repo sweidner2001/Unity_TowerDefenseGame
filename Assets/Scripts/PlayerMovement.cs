11e30d147febd93941a8362f6e9ffaa2a5cfcfef
Sebastian Weidner <s.weidner@oth-aw.de>
2025-05-16 16:29:35 +0200

    Add: Tilemap and Wirror Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
+    public int facingDirection = 1;
+
     public Rigidbody2D rb;
+    public Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     //void Start()
@@ -17,6 +20,23 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        // horizontal > 0 --> nach rechts laufen, aber Bild links ausgerichtet
+        // horizontal < 0 --> nach links laufen, aber Bild rechts ausgerichtet
+        if (horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();   
+        }
+
+        animator.SetFloat("horizontal", Mathf.Abs(horizontal));
+        animator.SetFloat("vertical", Mathf.Abs(vertical));
+
         rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
     }
 }
