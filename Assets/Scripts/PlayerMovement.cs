43a7b551760e56cb35e1d295b24779354a6eaea7
Sebastian Weidner <s.weidner@oth-aw.de>
2025-05-16 12:45:11 +0200

    Add: knight player - moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5;
+    public Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
-    {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
         
+    //}
+
+    // Update is called 50x frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
     }
 }
