3aba28e9700d35f96fdce05b6a82f3eb2734879e
Sebastian Weidner <s.weidner@oth-aw.de>
2025-07-10 18:38:10 +0200

    Entwurf TowerBuy

--- Assets/Scripts/Tower/TowerBase.cs
+++ Assets/Scripts/Tower/TowerBase.cs
@@ -1,17 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerBase : MonoBehaviour
 {
+    public GameObject towerBuy;
+    public GameObject towerTorch;
+    public GameObject towerWarrior;
+    public GameObject towerArcher;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //SpriteRenderer sr = GetComponent<SpriteRenderer>();
         //sr.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
+
+        ReplaceTower(towerArcher);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+
+
+    public void ReplaceTower(GameObject newTowerPrefab)
+    {
+        GameObject newTower = Instantiate(newTowerPrefab, transform.position, transform.rotation, transform.parent);
+
+        // Alle Childs von TowerBuy auf das neue Objekt verschieben
+        List<Transform> childs = new List<Transform>();
+        foreach (Transform child in transform)
+            childs.Add(child);
+        foreach (Transform child in childs)
+            child.SetParent(newTower.transform, true);
+
+        Destroy(gameObject);
     }
 }
